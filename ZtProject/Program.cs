575e9c2ef9352c4079688c7a03df6dfae8f5497d
glerkaan14 <glerkaan@yahoo.com>
2023-09-13 10:29:38 +0300

    Add Identity

--- ZtProject/Program.cs
+++ ZtProject/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using ZtProject.DataAccess.Data;
 using ZtProject.DataAccess.Repository;
 using ZtProject.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 
