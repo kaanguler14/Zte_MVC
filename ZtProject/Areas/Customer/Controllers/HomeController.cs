bd66df166884f3bee6795be6dc0dd117410173ac
glerkaan14 <glerkaan@yahoo.com>
2023-09-17 13:21:00 +0300

    Add Home Page

--- ZtProject/Areas/Customer/Controllers/HomeController.cs
+++ ZtProject/Areas/Customer/Controllers/HomeController.cs
@@ -15,10 +15,10 @@ namespace ZtProject.Areas.Customer.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IUnitOfWork _unitOfWork;
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
         {
             _logger = logger;
-            _unitOfWork = _unitOfWork;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
